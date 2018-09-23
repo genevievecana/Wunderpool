4ef6edb9db3ba901f48ef53b50144986a471a291
Gen Cana <yan2xcana@gmail.com>
2018-09-23 22:31:10 +0800

    added car item viewholder

---
+++ app/src/main/java/gencana/com/android/wunderpool/presentation/constant/enums/CarStatus.kt
@@ -0,0 +1,10 @@
+package gencana.com.android.wunderpool.presentation.constant.enums
+
+/**
+ * Created by Gen Cana on 23/09/2018
+ */
+
+enum class CarStatus{
+    GOOD,
+    UNACCEPTABLE
+}
--- app/src/main/java/gencana/com/android/wunderpool/presentation/ui/adapter/recyclerview/viewholder/CarItemViewHolder.kt
+++ app/src/main/java/gencana/com/android/wunderpool/presentation/ui/adapter/recyclerview/viewholder/CarItemViewHolder.kt
@@ -2,6 +2,7 @@ package gencana.com.android.wunderpool.presentation.ui.adapter.recyclerview.view
 
 import android.view.View
 import gencana.com.android.wunderpool.domain.model.Car
+import gencana.com.android.wunderpool.presentation.constant.enums.CarStatus
 import gencana.com.android.wunderpool.presentation.ui.adapter.recyclerview.BaseRecyclerMultiAdapter
 import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.item_car.view.*
@@ -13,7 +14,11 @@ internal class CarItemViewHolder(itemView: View)
 : BaseRecyclerMultiAdapter.BaseViewHolder<Car>(itemView) {
 
     override fun onBind(data: Car, itemClickPublisher: PublishSubject<Car>?) {
-        itemView.txt_name.text = data.address
+        itemView.txt_vin.text = data.vin
+        itemView.txt_address.text = data.address
+        itemView.txt_engine.text = data.engineType
+        itemView.cb_interior.isChecked = data.interior.equals(CarStatus.GOOD.name)
+        itemView.cb_exterior.isChecked = data.exterior.equals(CarStatus.GOOD.name)
     }
 
 }
